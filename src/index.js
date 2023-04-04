c73b334ad720be41afa448e60493496fd926d07b
Mahamud Arteh <Artehrush@gmail.com>
2023-04-04 14:46:03 +0100

    Read CSV input file and populate products array

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
+const inputFilePath = '../data/hb_test.csv';
+const outputFilePath = '../data/output.csv';
+
+let products = [];
+
+fs.createReadStream(inputFilePath)
+    .pipe(csv())
+    .on('data', (row) => {
+        products.push(row);
+        console.log(products);
+    })
